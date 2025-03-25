76f42da431f394fddb46a2af79fc32f2cba51a49
mdshakibsami <mdshakibsami21@gmail.com>
2025-03-26 01:29:03 +0600

    printing output in a new line in asm

---
+++ Assembly/Codes/Print output in new line/newline.asm
@@ -0,0 +1,36 @@
+.model small
+.stack 100h
+.code
+
+main proc
+    ;take a char input
+    mov ah,1  
+    int 21h
+    mov bl,al
+    
+    ;show new line
+    mov ah,2
+    mov dl,10
+    int 21h
+    ;also you have to return cret
+    mov ah,2
+    mov dl,13
+    int 21h
+
+    ;show char output
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    ;exit
+    exit:
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
+    
+
+; with out the cret return output will show like 
+; 5
+;    5 
+; cret return will remove the extra space and show output in new line properly
