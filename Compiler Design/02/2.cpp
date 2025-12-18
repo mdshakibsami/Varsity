2d8eff74d4408925c5214a7a5ec619cbb6725208
mdshakibsami <mdshakibsami21@gmail.com>
2025-12-18 10:23:18 +0600

    Write a C program to scan and count the number of characters, words, and lines in a file.

---
+++ Compiler Design/02/2.cpp
@@ -0,0 +1,51 @@
+/*
+Write a C program to scan and count the number of characters, words, and lines in a file.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream file;
+    char ch;
+    int characters = 0, words = 0, lines = 0;
+    bool inWord = false;
+
+    file.open("2.txt");
+
+    if (!file)
+    {
+        cout << "File not found!" << endl;
+        return 0;
+    }
+
+    while (file.get(ch))
+    {
+        characters++;
+
+        if (ch == '\n')
+            lines++;
+
+        if (ch == ' ' || ch == '\n' || ch == '\t')
+        {
+            inWord = false;
+        }
+        else if (!inWord)
+        {
+            inWord = true;
+            words++;
+        }
+    }
+
+    if (characters > 0)
+        lines++;
+
+    file.close();
+
+    cout << "Characters: " << characters << endl;
+    cout << "Words: " << words << endl;
+    cout << "Lines: " << lines << endl;
+
+    return 0;
+}
