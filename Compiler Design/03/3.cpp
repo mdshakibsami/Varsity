08a43b26359d2b7a6a639f2bba2fce7af89d364c
mdshakibsami <mdshakibsami21@gmail.com>
2025-12-18 10:33:18 +0600

    03 Write a C++ program to identify whether a given line is a comment or not.

---
+++ Compiler Design/03/3.cpp
@@ -0,0 +1,28 @@
+/*
+ * 03 Write a C++ program to identify whether a given line is a comment or not.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string line;
+    cout << "Enter a line: ";
+    getline(cin, line);
+
+    if (line.size() >= 2 &&
+        line[0] == '/' && line[1] == '/') {
+        cout << "Single-line comment" << endl;
+    }
+    else if (line.size() >= 4 &&
+             line[0] == '/' && line[1] == '*' &&
+             line[line.size() - 2] == '*' &&
+             line[line.size() - 1] == '/') {
+        cout << "Multi-line comment" << endl;
+    }
+    else {
+        cout << "Not a comment" << endl;
+    }
+
+    return 0;
+}
